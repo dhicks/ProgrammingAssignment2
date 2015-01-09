f7726e7e1a4d208ffabe6d2f88ea07bb247c0c1b
Dan Hicks <hicks.daniel.j@gmail.com>
2015-01-09 11:51:19 -0500

    assignment complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# The functions below provide support for a matrix class that can cache its own inverse
+# 
+# makeCacheMatrix(x=matrix()) : create a new instance with underlying matrix x
+# cacheSolve(x, ...) : calculate, cache, and return the inverse of x, using options ...
+#
+# To manipulate cache matrix x:  
+# x$set(m) : set the value of matrix x to matrix m, and clear inverse cache
+# x$get() : returns the matrix x
+# x$setinv(m) : manually set the inverse of x to matrix m
+# x$getinv() : returns the cached inverse of x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Create a new cache matrix
+  # Arguments: 
+  # x : the underlying matrix
+  inv <- null
+  set <- function(y) {
+    # Replace the underlying matrix and clear the cached inverse
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <- inverse
+  getinv <- function() inv
+  list(set = set, get = get, 
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, tolerance = 1.5e-8, ...) {
+  # Calculate, cache, and return the inverse of cache matrix x
+  # Arguments:
+  # tolerance : sets the tolerance on the inverse check
+  # ... : provides further arguments passed to the inverting function (solve())
+  # get the data from x
+  m <- x$get()
+  inv <- x$getinv()
+  # construct the identity matrix
+  id <- diag(ncol(x))
+  if(!is.null(inv) & isTRUE(all.equal(id, m %*% inv, tolerance))) {
+    # if the value in cache is not null and 
+    # we actually have the inverse (to within tolerance), 
+    # then return the value in cache
+    message('getting cached data')
+    return(inv)
+  }
+  # for further development: check here that x is invertible
+  # solve() with options to calculate the inverse
+  inv <- solve(m, ...)
+  # store the inverse in the cache
+  x$setinv(inv)
+  # and return it
+  inv
 }
